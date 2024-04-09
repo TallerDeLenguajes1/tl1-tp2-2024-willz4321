70df6d097549dacaf58a6876d4aa772d365a723d
willz4321 <willz4321@gmail.com>
2024-04-09 11:59:52 -0300

    primer commit

---
+++ trabajo practico numero 2/tp2_1_1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+  #define N 20
+        int i;
+
+        double vt[N];
+        for(i = 0;i<N; i++)
+        {
+        vt[i]=1+rand()%100;
+        printf("\n%f", vt[i]);
+        }
+
+}
